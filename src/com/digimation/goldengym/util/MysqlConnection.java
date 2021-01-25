199d0afc22454be2e73c8ff00f21b1580e7e0ab4
TruongTra <nhattruong25041999@gmail.com>
2021-01-25 21:38:05 +0700

    update connection

--- src/com/digimation/goldengym/util/MysqlConnection.java
+++ src/com/digimation/goldengym/util/MysqlConnection.java
@@ -8,7 +8,7 @@ public class MysqlConnection {
 
 	private static String username = "n1mkrppqiji3qprg";
 	private static String password = "m7ahtpc0cwk3igpl";
-	private static String URL = "mysql://n1mkrppqiji3qprg:m7ahtpc0cwk3igpl@z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tvmiawih19nlst2p";
+	private static String URL = "jdbc:mysql://n1mkrppqiji3qprg:m7ahtpc0cwk3igpl@z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tvmiawih19nlst2p";
 	private static String driverClass = "com.mysql.cj.jdbc.Driver";
 
 	public static Connection getConnection() {
